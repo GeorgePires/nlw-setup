7e8015fa9520d2cd1a4516632d47d7adf4d1353a
GeorgePires <georgeasp.pi@gmail.com>
2023-01-21 00:57:43 -0300

    :sparkles: feat/ create function add day to register day

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 const form = document.querySelector("#form-habits")
 const nlwSetup = new NLWSetup(form)
+const btn = document.querySelector("header button")
 
-const data = {
-  run: ["01-01", "01-02", "01-03", "01-04", "01-05", "01-06"],
-  water: ["01-01", "01-03", "01-06"],
-  food: ["01-03", "01-02", "01-06"],
-  english: ["01-01", "01-02", "01-04", "01-06"],
+//const data = {
+//  run: ["01-01", "01-02", "01-03", "01-04", "01-05", "01-06"],
+//  water: ["01-01", "01-03", "01-06"],
+//  food: ["01-03", "01-02", "01-06"],
+//  english: ["01-01", "01-02", "01-04", "01-06"],
+//}
+
+//nlwSetup.setData(data)
+//nlwSetup.load()
+
+btn.addEventListener("click", add)
+
+function add() {
+  const today = new Date().toLocaleDateString().slice(0, 5)
+  const dayExists = nlwSetup.dayExists(today)
+
+  if (dayExists) {
+    alert("Day already exists ❌")
+    return
+  }
+
+  alert("Registered day ✅")
+  nlwSetup.addDay(today)
 }
-
-nlwSetup.setData(data)
-nlwSetup.load()
